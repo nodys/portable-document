9b9907eb1a26fe319c175f47afaf03de246cccf6
Sacha Bejaud <sacha.bejaud@novadiscovery.com>
2020-12-10 10:21:04 +0100

    feat: use annotations data

---
+++ src/assets/data.ts
@@ -0,0 +1,16 @@
+const x = 0,
+  y = 0,
+  w = 100,
+  h = 100;
+
+const sampleAnchor = [x, y, w, h];
+
+export const annotations = [
+  {
+    id: "3ac397c8-4ae4-4dbb-bc98-2eeb23a1115c",
+    anchors: [sampleAnchor, [300, 300, 20, 5]],
+    data: {
+      someEndUserAppProps: "ok"
+    }
+  }
+];
