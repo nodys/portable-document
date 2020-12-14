0e5eff70e4e47b7c04f90ca11711c65f435af25d
Sacha Bejaud <sacha.bejaud@novadiscovery.com>
2020-12-14 16:25:32 +0100

    feat: add annotation schemas

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -3,9 +3,17 @@ const x = 0,
   w = 100,
   h = 100;
 
-const sampleAnchor = [x, y, w, h];
+const sampleAnchor: Anchor = [x, y, w, h];
 
-export const annotations = [
+export type Anchor = [number, number, number, number];
+
+export interface Annotation {
+  id: string;
+  anchors: Anchor[];
+  data: unknown;
+}
+
+export const annotations: Annotation[] = [
   {
     id: "3ac397c8-4ae4-4dbb-bc98-2eeb23a1115c",
     anchors: [sampleAnchor, [300, 300, 20, 5]],
